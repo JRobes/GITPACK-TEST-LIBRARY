17882ca2f066e924bb15e6b2b1b266bfb4404183
javier.robes <javier.robes@CNU1523JPC.sev.alestis>
2018-09-04 10:47:06 +0200

    Un cambio posterior

--- GITPACK-TEST-LIBRARY/src/aero/library/Factory.java
+++ GITPACK-TEST-LIBRARY/src/aero/library/Factory.java
@@ -3,6 +3,6 @@ package aero.library;
 public class Factory {
 
 	public void createFunction(String s) {
-		System.out.println("Desde dentro de la fuction:\t"+s);
+		System.out.println("Desde dentro de la fuction Factory.create():\t"+s);
 	}
 }
